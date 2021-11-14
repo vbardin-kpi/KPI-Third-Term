66dc93d0bf723681f6e98b8bc4fd3e275d1d67b2
Vladislav Bardin <vbardin810@gmail.com>
2021-11-14 10:23:39 +0200

    Add database lab-2
    Added:
    1. SQL scripts for database creating and filling out
    2. Lab Report

---
+++ DB/lab2/sql/CreateUserAndDB.sql
@@ -0,0 +1,9 @@
+﻿-- Create a user and a database.
+-- Grant him a full access to the database.
+
+CREATE USER SC_owner WITH PASSWORD 'SC_owner';
+
+CREATE DATABASE "SCompany-Main-DB" owner SC_owner;
+ALTER DATABASE "SCompany-Main-DB" SET TIMEZONE = 'UTC';
+
+GRANT ALL PRIVILEGES ON DATABASE "SCompany-Main-DB" TO SC_owner;
